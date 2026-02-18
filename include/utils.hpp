1d03b8f40fff47bdced74ce5a20486082b761e02
Jake Baker <jake.baker@sony.com>
2026-02-17 22:06:27 -0600

    cleanup strutcture a bit

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,3 +1,8 @@
 #pragma once
+#include <cstddef>
 
-void function();
+constexpr float PI = 3.141592;
+constexpr size_t WINDOW_WIDTH = 1024;
+constexpr size_t WINDOW_HEIGHT = 768;
+constexpr size_t TEXTURE_WIDTH = 800;
+constexpr size_t TEXTURE_HEIGHT = 600;
